195e4a4797640e6f32b2569517d2550d0f486d81
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-15 23:31:43 -0800

    Begin adding TODO functions to myalloc program.

--- myalloc.c
+++ myalloc.c
@@ -1,20 +1,33 @@
 #include <stdlib.h>   // NULL
 #include <sys/mman.h> // mmap()
+#include <stdbool.h>  // bool
+#include <stdio.h>    // printf
+
+struct block
+{
+    struct block *next;
+    int size;   // bytes
+    int in_use; // boolean
+};
+
 /**
  *  Allocate a given number of bytes & return a pointer to it.
  */
 void *myalloc(int size)
 {
     void *heap;
+    bool is_first_call = 0;
 
-    // if this is the first call
-    // mmap space
-    // build a linked list node inside new space, indicating size and "in use" status
-    if (0)
+    if (is_first_call)
+    {
+        // mmap space
         heap = mmap(
             NULL, 1024, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
 
-    // traverse linked list (loop)
+        // build a linked list node inside new space, indicating size and "in use" status
+    }
+
+    // traverse (loop over) linked list
     // find first node not "in use"
     // &&
     // big enough to hold given size + padding
@@ -35,9 +48,37 @@ void myfree(void *p)
     return;
 }
 
+void print_data(void)
+{
+    struct block *b = head;
+
+    if (b == NULL)
+    {
+        printf("[empty]\n");
+        return;
+    }
+
+    while (b != NULL)
+    {
+        // Uncomment the following line if you want to see the pointer values
+        printf("[%p:%d,%s]", b, b->size, b->in_use ? "used" : "free");
+        // printf("[%d,%s]", b->size, b->in_use ? "used" : "free");
+        if (b->next != NULL)
+        {
+            printf(" -> ");
+        }
+
+        b = b->next;
+    }
+
+    printf("\n");
+}
+
 int main(int argc, char const *argv[])
 {
     // allocate space for 5 ints
     int *p = myalloc(sizeof(int) * 5);
+    printf("%p\n", p);
+
     return 0;
 }
