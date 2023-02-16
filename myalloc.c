6f6750189e72460475a0457200b2cfdcdc389188
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-15 19:05:20 -0800

    Add provided mmap function.

--- myalloc.c
+++ myalloc.c
@@ -1,22 +1,30 @@
-#include <stdlib.h>
+#include <stdlib.h>   // NULL
+#include <sys/mman.h> // mmap()
 /**
  *  Allocate a given number of bytes & return a pointer to it.
  */
 void *myalloc(int size)
 {
+    void *heap;
+
     // if this is the first call
     // mmap space
     // build a linked list node inside new space, indicating size and "in use" status
+    if (0)
+        heap = mmap(
+            NULL, 1024, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
 
     // traverse linked list (loop)
     // find first node not "in use"
     // &&
-    // is big enough to hold given size + padding
+    // big enough to hold given size + padding
 
     // if found
     // mark "in use"
     // return pointer to user data after linked list node (+ padding)
-    return NULL;
+
+    // else return NULL
+    return ((void *)0);
 }
 
 /**
@@ -29,5 +37,7 @@ void myfree(void *p)
 
 int main(int argc, char const *argv[])
 {
+    // allocate space for 5 ints
+    int *p = myalloc(sizeof(int) * 5);
     return 0;
 }
